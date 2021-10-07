ce600b6367fe9d1ad12a9a36d875b63b2ad7ff81
oonggee <90182649+oonggee@users.noreply.github.com>
2021-10-07 15:10:26 +0700

    Update covid.py

--- covid.py
+++ covid.py
@@ -2,7 +2,6 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import plotly.express as px
-from plotly.subplots import make_subplots
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 
